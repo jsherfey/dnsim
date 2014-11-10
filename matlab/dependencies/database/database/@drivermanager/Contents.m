27618b233e8978e66454e36927b5a861abb75060
Jason Sherfey <jssherfey@gmail.com>
2014-11-10 05:15:38 -0500

    removed database toolbox because without license it doesnt work

--- matlab/dependencies/database/database/@drivermanager/Contents.m
+++
@@ -1,9 +0,0 @@
-%  Database Toolbox Drivermanager Object.
-%  Copyright 1984-2002 The MathWorks, Inc.
-%
-%  Database Toolbox Drivermanager functions:
-%
-%  drivermanager  - Construct database drivermanager object.
-%  get            - Get database drivermanager properties.
-%  set            - Set database drivermanager properties.
-
--- matlab/dependencies/database/database/@rsmd/Contents.m
+++
@@ -1,8 +0,0 @@
-%  Database Toolbox Resultset MetaData Object.
-%  Copyright 1984-2002 The MathWorks, Inc.
-%
-%  Database Toolbox Resultset MetaData functions:
-%
-%  rsmd  - Construct resultset metadata object.
-%  get   - Get resultset metadata properties.
-
