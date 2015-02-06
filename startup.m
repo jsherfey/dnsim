a73c132dfc06ef580427194cbcdbf54a1b13dd02
Austin Soplata <austin.soplata@gmail.com>
2015-02-06 15:50:39 -0500

    On branch local_branch stupid change

--- startup.m
+++ startup.m
@@ -8,6 +8,7 @@ end
 
 % randomize seed
 % lulz
+% this is for local_branch branc
 % see: http://www.mathworks.com/help/matlab/math/updating-your-random-number-generator-syntax.html
 try
   rng('shuffle'); % - exception: rand in parfor does not use this new seed
